2b83f254694f7fa4ba74d65ee544da40062b19b8
Steven Combs <scombs@pillartechnology.com>
2019-03-26 16:42:12 -0500

    Refactored from if else to a when statement

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -3,8 +3,10 @@ interface FizzBuzz {
 
 
     fun Int.fizzBuzz(): String {
-        return if(this%3==0) "Fizz"
-        else if(this%5==0) "Buzz"
-        else this.toString()
+        return when {
+            this%3==0 -> "Fizz"
+            this%5==0 -> "Buzz"
+            else -> this.toString()
+        }
     }
 }
