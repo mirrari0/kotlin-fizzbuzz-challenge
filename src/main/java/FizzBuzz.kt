e507cdfceea30eb0da2567561b6829591a58f6a5
Steven Combs <scombs@pillartechnology.com>
2019-04-03 15:47:54 -0500

    Added three and five scenario (missed me a commit :( )

--- src/main/java/FizzBuzz.kt
+++ src/main/java/FizzBuzz.kt
@@ -1,5 +1,11 @@
 interface FizzBuzz {
 
 
-    fun Int.playGame(): String = this.toString()
+    fun Int.playGame(): String {
+        return when {
+            this % 3 == 0 -> "fizz"
+            this % 5 == 0 -> "buzz"
+            else -> this.toString()
+        }
+    }
 }
--- src/test/java/FizzBuzzTest.kt
+++ src/test/java/FizzBuzzTest.kt
@@ -7,13 +7,30 @@ class FizzBuzzTest: FizzBuzz {
 
     @Test
     fun `when given a number, returns number`() {
-        val num = getNumber(false, false)
+        val num = getNumber(false, false, 1)
         assertEquals(num.toString(), num.playGame())
+    }
+
+    @Test
+    fun `when given a multiple of three and not five, returns fizz`() {
+        assertEquals("fizz", getNumber(true, false, 3).playGame())
+    }
+
+    @Test
+    fun `when given a multiple of five and not three, returns buzz`() {
+        assertEquals("buzz", getNumber(false, true, 5).playGame())
     }
 
 
 
-    fun getNumber(three: Boolean, five: Boolean): Int{
-        return (Math.random()*100).toInt()
+    private fun getNumber(three: Boolean, five: Boolean, multiplier: Int): Int{
+        var num =  (Math.random()*100).toInt()
+        while(true) {
+            val start = num
+            if(!five && num%5 ==0) num++
+            if(!three && num%3==0) num++
+            if(num == start) break
+        }
+        return num * multiplier
     }
 }
